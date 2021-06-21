0f8bfa25fdf391bcafe5e2e4a680676d9406eccf
Aliwebs <11ASkhankausar@gmail.com>
2021-06-21 13:13:08 +0100

    changes

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,6 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
     content = models.TextField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True, auto_now=True)
